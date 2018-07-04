be3565c99ace0750c1eb765afc4931d5649272b4
chenkexi <59599339@qq.com>
2018-07-04 19:02:00 +0800

    modiy black board

--- BT/BaseNode/BlackBoard.py
+++ BT/BaseNode/BlackBoard.py
@@ -3,41 +3,41 @@ __all__ = ['BlackBoard']
 
 class BlackBoard(object):
     def __init__(self):
-        self._base_memory = {}
-        self._tree_memory = {}
+        self._normal_data = {}
+        self._tree_data = {}
 
-    def _get_tree_memory(self, tree_scope):
-        if (tree_scope not in self._tree_memory):
-            self._tree_memory[tree_scope] = {
-                'node_memory': {},
+    def _get_tree_data(self, tree_id):
+        if (tree_id not in self._tree_data):
+            self._tree_data[tree_id] = {
+                'node_data': {},
                 'open_nodes': []
             }
 
-        return self._tree_memory[tree_scope]
+        return self._tree_data[tree_id]
 
-    def _get_node_memory(self, tree_memory, node_scope):
-        memory = tree_memory['node_memory']
+    def _get_node_data(self, tree_data, node_id):
+        data = tree_data['node_data']
 
-        if (node_scope not in memory):
-            memory[node_scope] = {}
+        if (node_id not in data):
+            data[node_id] = {}
 
-        return memory[node_scope]
+        return data[node_id]
 
-    def _get_memory(self, tree_scope, node_scope):
-        memory = self._base_memory
+    def _get_data(self, tree_id, node_id):
+        data = self._normal_data
 
-        if (tree_scope is not None):
-            memory = self._get_tree_memory(tree_scope)
+        if (tree_id is not None):
+            data = self._get_tree_data(tree_id)
 
-            if (node_scope is not None):
-                memory = self._get_node_memory(memory, node_scope)
+            if (node_id is not None):
+                data = self._get_node_data(data, node_id)
 
-        return memory
+        return data
 
-    def set(self, key, value, tree_scope=None, node_scope=None):
-        memory = self._get_memory(tree_scope, node_scope)
-        memory[key] = value
+    def set(self, key, value, tree_id=None, node_id=None):
+        data = self._get_data(tree_id, node_id)
+        data[key] = value
 
-    def get(self, key, tree_scope=None, node_scope=None):
-        memory = self._get_memory(tree_scope, node_scope)
-        return memory.get(key)
+    def get(self, key, tree_id=None, node_id=None):
+        data = self._get_data(tree_id, node_id)
+        return data.get(key)
